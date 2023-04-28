1fde8cf9914e9d610b1a00854b6f9e24d4a58345
Mountains-CSS <126298437+Mountains-CSS@users.noreply.github.com>
2023-04-29 07:18:20 +0800

    Create JavaScript.js

---
+++ JavaScript.js
@@ -0,0 +1,9 @@
+function imageOverlay() {
+    var overlay = document.getElementById('image-overlay')
+    overlay.classList.add('active')
+}
+
+function closeImageOverlay() {
+    var overlay = document.getElementById('image-overlay')
+    overlay.classList.remove('active')
+}
