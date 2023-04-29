70c41742e07a49295187699d94595152e0d28bc3
Mountains-CSS <126298437+Mountains-CSS@users.noreply.github.com>
2023-04-29 16:55:13 +0800

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -7,3 +7,15 @@ function closeImageOverlay() {
     var overlay = document.getElementById('image-overlay')
     overlay.classList.remove('active')
 }
+
+function openOverlay(imageUrl) {
+    var overlay = document.getElementById("my-overlay");
+    var overlayImage = overlay.querySelector(".my-overlay-image");
+    overlayImage.src = imageUrl;
+    overlay.classList.add("active");
+}
+
+function closeOverlay() {
+    var overlay = document.getElementById("my-overlay");
+    overlay.classList.remove("active");
+}
