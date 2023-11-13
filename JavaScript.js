ea92ece05b4428fab96b108baa70e46878849a79
Mountains-CSS <126298437+Mountains-CSS@users.noreply.github.com>
2023-11-13 15:14:29 +0800

    Delete JavaScript.js

--- JavaScript.js
+++
@@ -1,9 +0,0 @@
-function openImageOverlay() {
-    var overlay = document.getElementById('image-meaning')
-    overlay.classList.add('active')
-}
-
-function closeImageOverlay() {
-    var overlay = document.getElementById('image-meaning')
-    overlay.classList.remove('active')
-}
