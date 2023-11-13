f8e14312c3173fe43eb1202155e16590a945ee6c
Mountains-CSS <126298437+Mountains-CSS@users.noreply.github.com>
2023-11-13 13:50:52 +0800

    Add files via upload

---
+++ personal-dcis-app/src/pages/Home.js
@@ -0,0 +1,14 @@
+import classes from './pages.module.css';
+
+function Home() {
+      return (
+            <div>
+                  <h1 className={classes.headings}>
+                        Home
+                  </h1>
+
+            </div>
+      )
+}
+
+export default Home;
